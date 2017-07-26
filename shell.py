d9e734802611a2b22e6acd8eb86edb9cccf1b9f5
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-07-26 09:49:09 -0500

    Imported the load_inventory from disk

--- shell.py
+++ shell.py
@@ -1,10 +1,10 @@
-from core import only_items
+from disk import load_inventory
 def main():
     print('Hello, welcome to Party Room Rentals.\n Here is what we have for today.')
-    items = only_items()
+    items = load_inventory()
     print(items)
     answer = input('Which one would you like to look at?')
-    
+
 
 
 if __name__ == '__main__':
