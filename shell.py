4455280d94a7836d0b3bfc6bf0c14ef11b3f2311
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-07-26 11:42:19 -0500

    Imported more functions and started to add more choice for the user

--- shell.py
+++ shell.py
@@ -1,11 +1,20 @@
-from disk import load_inventory
+from disk import load_inventory, load_item
+from core import get_item, item_messages
+def rules():
+    print('Hello, welcome to Party Room Rentals.\n\n')
+    print ('We would like you to know some things before you rent an item.\n\n')
+    print('The price of all items is the price per hour\n\nYou also have to pay a 10% deposit of replacement value.\n\n')
+    print('Here is what we have for today.\n\n')
 def main():
-    print('Hello, welcome to Party Room Rentals.\n Here is what we have for today.')
-    items = load_inventory()
-    print(items)
-    answer = input('Which one would you like to look at?')
-
-
+    rules()
+    inventory = load_inventory()
+    print(inventory)
+    product = input('Which one would you like to look at? ').strip()
+    store = load_item()
+    valid = get_item(store, product)
+    print(valid)
+    message = item_messages(inventory, product)
+    print(message)
 
 if __name__ == '__main__':
     main()
