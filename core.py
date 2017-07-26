af9655e35a6529a9babc42a1753bf2badc714802
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-07-26 09:43:13 -0500

    Made a Function That returns Only the items of the invenory

---
+++ core.py
@@ -0,0 +1,11 @@
+def only_items():
+    ''' _> [str]
+    return only the items from the inventory
+    '''
+    items = ['Chairs',
+    'Tables',
+    'Bouncy House',
+    'Flowers',
+    'Music Sytem'
+    ]
+    return items
