c264fef00b257d8e6befbcb51691d5ddb5647c4d
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-07-26 11:36:46 -0500

    Added a load_item() to return info of the item choosen

--- disk.py
+++ disk.py
@@ -10,3 +10,21 @@ def load_inventory():
         pieces = element.split(', ')
         inventory.append(pieces[0])
     return inventory
+
+
+def load_item():
+    """ -> list[list]
+    returns the info of the item choosen
+    """
+    with open("inventory.txt", 'r') as file:
+        file.readline()
+        items = file.readlines()
+    products = []
+    for purchases in items:
+        individual = purchases.split(', ')
+        products.append([
+            individual[0],
+            individual[1],
+            individual[2].strip()
+        ])
+    return products
