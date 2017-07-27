408f7a8ac822fe0245b1fb802b3e00d98a260b79
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-07-27 09:54:00 -0500

    Wrote successful pytest for item_messages i then added abug and fixed it. I am going to keep writing the test and after i am going to advance more in the project as a whole

--- test_core.py
+++ test_core.py
@@ -3,3 +3,11 @@ def test_get_item():
     actual = core.get_item([['Toys', 20, 2.00]], 'Toys')
     expect = ['Toys', 20, 2.00]
     assert expect == actual
+
+def test_item_messages():
+    actual = core.item_messages([['Toys', 20, 2.00]], 'Toys')
+    expect = 'Here is the item, followed by the quantity left and the price per item'
+    assert expect == actual
+    actual = core.item_messages([['Toys', 20, 2.00]], 'Wheels')
+    expect = 'We are sorry, but we do not have the item for rent.'
+    assert expect == actual
