18743505818c40428cd86b0021a195e7144c6803
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-07-31 15:26:58 -0500

    Added is_on_time function to see if the customer bought the item on time and if they did not bring it on time, the it will return the difference

--- core.py
+++ core.py
@@ -53,3 +53,58 @@ def tax_of(money):
     tax = (money) * .07
     total = money + tax
     return '{:0.2f}'.format(total)
+
+# def check_time(inventory,timing_now, number):
+#     ''' int _> float
+#     Returns the difference in time to see 
+#     if the customer owes more or not
+#     '''
+#     msg = 'Sorry there was an error'
+#     for items in inventory:
+#         if number in items:
+#             time = items[5]
+#             diff = timing_now - time
+#             # Divided by 60 twice to get the mintues and hours
+#             difference = diff.total_seconds()/60/60
+#             hours = items[4]
+#             if float(difference) > float(hours):
+#                 x = float(difference) - float(hours)
+#                 return 'You owe', x, 'hours'
+#             return difference
+#     return msg
+
+def is_on_time(inventory, timing_now, number):
+    ''' list[str]], date.time, int _> float
+    Returns the number of hours different
+    '''
+    bad_msg = 'Sorry, there was an error during the transaction please try again.'
+    msg = "You did not exceed the hours, unfortunatly we do not give refunds.  "
+    for items in inventory:
+        if number in items:
+            diff = timing_now - items[5]
+            hours_diff = diff.total_seconds()/60/60
+            if hours_diff < timing_now:
+                total_hrs = hours_diff - timing_now
+                return msg
+            else:
+                return float(total_hrs)
+    return bad_msg
+
+
+
+# >>> from datetime import datetime
+# >>> st = datetime.now()
+# >>> end = datetime.now()
+# >>> end - st
+# datetime.timedelta(0, 10, 646110)
+# >>> d = end - st
+# >>> d.days
+# 0
+# >>> d.min
+# datetime.timedelta(-999999999)
+# >>> d.total_seconds
+# <built-in method total_seconds of datetime.timedelta object at 0x7f9ca9b5f968>
+# >>> d.total_seconds()
+# 10.64611
+# >>> d.total_seconds() / 60 / 60
+# 0.002957252777777778
