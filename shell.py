d14bcd62c9bfcd9271d04f8eeda431f3c4271a1e
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-07-31 11:52:47 -0500

    Added a branch so the customer can return the item

--- shell.py
+++ shell.py
@@ -1,7 +1,8 @@
-from disk import load_inventory, load_item, cost_of, replacement_of
+from disk import load_inventory, load_item, replacement_of
+from disk import update_history, transaction_num, cost_of
 from core import get_item, item_messages, tax_of, rent_time
 def rules():
-    print('Hello, welcome to Party Room Rentals.\n\n')
+    print('\nHello, welcome to Party Room Rentals.\n\n')
     print ('We would like you to know some things before you rent an item.\n\n')
     print('The price of all items is the price per hour\n\nYou also have to pay a 10% deposit of replacement value.\n\n')
     print('Here is what we have for today.\n\n')
@@ -16,18 +17,28 @@ def store():
     print(message)
     amount = input('\nHow many would you like? ').strip()
     hours = input('\nFor how many hour(s) would you like to rent this item?')
-    money = cost_of(float(amount), product, float(hours))
+    money = cost_of(float(amount), product, hours)
     tax = tax_of(float(money))
     print('We would like to remind you that there is a 10% replacement value required fee. \n')
-    print('You got ', amount, 'items of', product.title(), '\n')
     replacing = replacement_of(product, amount)
     print('Ok your total with tax is $', float(money), '\nYou also have to pay a deposit of $', float(replacing))
-    time = rent_time(product)
-    print(time)
+    time_out = rent_time(product)
     renting = float(money) + float(replacing)
     print('Your total for today is $', renting)
+    number = transaction_num(product)
+    update_history(str(number), float(money), product, int(amount), float(hours), time_out, replacing)
+    print('Thanks for stopping by your number is ', number, ' you will need it when you come back.')
+def returning():
+    number = input('What is your number? ')
 def main():
-    rules()
-    store()
+    print('1.\tCheck out store\n2.\tReturn items rented\n')
+    answer = input('What would you like to do today (Please enter number) ?  ')
+    if answer == '1':
+        rules()
+        store()
+    elif answer == '2':
+        return None
+    else:
+        print("Sorry invalid answer.")
 if __name__ == '__main__':
     main()
