7e11a9b0fc0324a29ae6518dde2c0c7437936654
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-08-08 09:33:45 -0500

    Added a funciton that will calculate the total revenue and then will be used in the shell

--- disk.py
+++ disk.py
@@ -22,11 +22,7 @@ def load_item():
     products = []
     for purchases in items:
         individual = purchases.split(', ')
-        products.append([
-            individual[0],
-            individual[1],
-            individual[2].strip()
-        ])
+        products.append([individual[0], individual[1], individual[2].strip()])
     return products
 
 
@@ -48,6 +44,7 @@ def cost_of(amount, item, hours):
             return total
     return msg
 
+
 def replacement_of(item, amount):
     ''' str, int _> float
     Function will recive and item and the amount rented
@@ -66,15 +63,20 @@ def replacement_of(item, amount):
             return '{:0.2f}'.format(value)
     return msg
 
+
 def update_history(number, payed, item, amount, hours_rent, time, deposit):
     ''' str, float, float _> None
     Function will update the history text file with all the 
     parameters provided
     '''
-    msg = str(number) + ', ' + str(payed) + ', ' + str(item) + ', ' + str(amount) + ', ' + str(hours_rent) + ', ' + str(time) + ', ' + str(deposit) + '\n'
+    msg = str(number) + ', ' + str(payed) + ', ' + str(item) + ', ' + str(
+        amount) + ', ' + str(hours_rent) + ', ' + str(time) + ', ' + str(
+            deposit) + '\n'
     with open('history.txt', 'a') as file:
         file.write(msg)
     return None
+
+
 def transaction_num(name):
     ''' str _> int
     Function will get the name after every
@@ -87,6 +89,7 @@ def transaction_num(name):
         numbers += 1
     return int(numbers)
 
+
 def load_history():
     """ -> list[list]
     returns the info of the item choosen
@@ -98,15 +101,11 @@ def load_history():
     for purchases in items:
         individual = purchases.split(', ')
         products.append([
-            individual[0],
-            individual[1],
-            individual[2],
-            individual[3],
-            individual[4],
-            individual[5],
-            individual[6].strip()
+            individual[0], individual[1], individual[2], individual[3],
+            individual[4], individual[5], individual[6].strip()
         ])
     return products
+
 
 def in_inventory():
     ''' _> inventory
@@ -119,8 +118,10 @@ def in_inventory():
         lines = file.readlines()
     for line in lines:
         split_string = line.strip().split(', ')
-        left.append([(split_string[0]), (split_string[1]), (split_string[2]), (split_string[3])])
+        left.append([(split_string[0]), (split_string[1]), (split_string[2]),
+                     (split_string[3])])
     return left
+
 
 def take_away(item_rented, amount):
     """ 
@@ -135,15 +136,16 @@ def take_away(item_rented, amount):
             if int(amount) > int(item[1]):
                 return False
             else:
-                item[1] = int(item[1]) - int(amount)
+                item[1] = int(item[1]) - (int(amount) // 2)
         item[2] = str(item[2])
         item[1] = str(item[1])
         str_l.append(', '.join(item))
         message = '\n'.join(str_l)
 
-    with open('inventory.txt', 'w') as file: 
+    with open('inventory.txt', 'w') as file:
         file.write(message)
     return True
+
 
 def in_history():
     """ 
@@ -157,43 +159,77 @@ def in_history():
         lines = file.readlines()
     for line in lines:
         split_string = line.strip().split(', ')
-        left.append([(split_string[0]), (split_string[1]), (split_string[2]), (split_string[3]), (split_string[4]), (split_string[5]), (split_string[6])])
+        left.append([(split_string[0]), (split_string[1]), (split_string[2]),
+                     (split_string[3]), (split_string[4]), (split_string[5]),
+                     (split_string[6])])
     return left
 
-def add_back(number, amount):
-    """ 
-    Function adds the amount borrowed back to the inventory
-    """
-    str_l = ['item:, cost:, amount of items avalaible:']
-    items_left = in_history()
-    for item in items_left:
-        if item[0] != number:
-                return False
-        else:
-            item[3] = int(item[3]) + int(amount)
-        item[2] = str(item[2])
-        item[3] = str(item[3])
-        str_l.append(', '.join(item))
-        message = '\n'.join(str_l)
 
-    with open('inventory.txt', 'w') as file: 
-        file.write(message)
-    return True
-
-
-def return_amount_borrowed(number):
+def sum_revenue():
+    """ _> float
+    Function will get the total revenue when called
+    in the manager branch in the shell
     """
-    Function returns the amount the customer borrowed
-    once there number is given 
-    """
-    with open('history.txt') as file:
+    with open('history.txt', 'r'): as file:
         file.readline()
-        elements = file.readlines()
-    msg = 'Sorry there was a error in during the transaction'
-    for items in elements:
-        pieces = items.split(', ')
-        if number in pieces:
-            return int(pieces[3])
-        else:
-            return False
-    return msg
+        element = file.realines()
+    total = 0
+    for item in element:
+        pieces = item.split(', ')
+        deposit = int(pieces[6])
+        total += deposit
+    return total
+
+
+# def add_back(number, amount):
+#     """
+#     Function adds the amount borrowed back to the inventory
+#     """
+#     str_l = ['item:, cost:, amount of items avalaible:']
+#     items_left = in_history()
+#     for item in items_left:
+#         if item[0] != number:
+#             return False
+#         else:
+#             item[3] = int(item[3]) + int(amount)
+#         item[2] = str(item[2])
+#         item[3] = str(item[3])
+#         str_l.append(', '.join(item))
+#         message = '\n'.join(str_l)
+
+#     with open('inventory.txt', 'w') as file:
+#         file.write(message)
+#     return True
+
+# def return_amount_borrowed(number):
+#     """
+#     Function returns the amount the customer borrowed
+#     once there number is given
+#     """
+#     with open('history.txt') as file:
+#         file.readline()
+#         elements = file.readlines()
+#     msg = 'Sorry there was a error in during the transaction'
+#     for items in elements:
+#         pieces = items.split(', ')
+#         if number in pieces:
+#             return int(pieces[3])
+#         else:
+#             return False
+#     return msg
+
+# def after_returning_history(number):
+#     """
+#     Function will get a number and look
+#     for it in the history file, and if it
+#     is found, it will make the deposit be zero
+#     """
+#     with open('history.txt', 'r') as file:
+#         file.readline()
+#         items = file.readlines()
+#     for products in items:
+#         if number in products:
+#             pieces.products.split(', ')
+#             products[6] = 0.0
+#             return products
+#     return None
