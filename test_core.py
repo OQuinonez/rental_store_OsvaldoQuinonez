708a9f3d803dfdbe740797f130d4bcc8fa8bead9
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-08-08 09:46:51 -0500

    Erased a funciton that wont be tested

--- test_core.py
+++ test_core.py
@@ -1,8 +1,11 @@
 import core
+
+
 def test_get_item():
     actual = core.get_item([['Toys', 20, 2.00]], 'Toys')
     expect = ['Toys', 20, 2.00]
     assert expect == actual
+
 
 def test_item_messages():
     actual = core.item_messages([['Toys', 20, 2.00]], 'Toys')
@@ -12,10 +15,12 @@ def test_item_messages():
     expect = 'We are sorry, but we do not have the item for rent.'
     assert expect == actual
 
+
 def test_tax_of():
     actual = core.tax_of(1.00)
     expect = '1.07'
     assert expect == actual
+
 
 # def test_rent_time():
 #     actual = core.rent_time('Bob')
