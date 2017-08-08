f326d61f0d20a110044827a57e839ac0218626d5
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-08-08 09:30:59 -0500

    Added more to the manager branch to dislpay the total revenue to the user

--- shell.py
+++ shell.py
@@ -2,14 +2,22 @@ import core
 import disk
 import time
 from datetime import datetime
+
+
 def rules():
     print('\nHello, welcome to Party Room Rentals.\n\n')
-    print ('We would like you to know some things before you rent an item.\n\n')
-    print('The price of all items is the price per hour\n\nYou also have to pay a 10% deposit of replacement value.\n\n')
+    print('We would like you to know some things before you rent an item.\n\n')
+    print(
+        'The price of all items is the price per hour\n\nYou also have to pay a 10% deposit of replacement value.\n\n'
+    )
     print('Here is what we have for today.\n\n')
+
+
 def inventory():
     inventory = disk.load_inventory()
     print(inventory)
+
+
 def valid_product():
     product = input('Which one would you like to look at? ').strip()
     store = disk.load_item()
@@ -26,19 +34,31 @@ def valid_product():
     if disk.take_away(product, amounts) == False:
         print('Sorry, we do not have that many items.')
     else:
-        print('We would like to remind you that there is a 10% replacement value required fee. \n')
+        print(
+            'We would like to remind you that there is a 10% replacement value required fee. \n'
+        )
         replacing = disk.replacement_of(product, amounts)
-        print('Ok your total with tax is $', float(money), '\nYou also have to pay a deposit of $', float(replacing))
+        print('Ok your total with tax is $',
+              float(money), '\nYou also have to pay a deposit of $',
+              float(replacing))
         time_out = core.rent_time(product)
         renting = float(money) + float(replacing)
         print('Your total for today is $', renting)
         number = disk.transaction_num(product)
-        disk.update_history(str(number), float(money), product, int(amounts), float(hours), time_out, replacing)
-        print('Thanks for stopping by your number is ', number, ' you will need it when you come back.')
+        disk.update_history(
+            str(number),
+            float(money), product,
+            int(amounts), float(hours), time_out, replacing)
+        print('Thanks for stopping by your number is ', number,
+              ' you will need it when you come back.')
+
+
 def store():
     rules()
     inventory()
     valid_product()
+
+
 def returning():
     inventory = disk.load_inventory()
     history = disk.load_history()
@@ -53,15 +73,37 @@ def returning():
     else:
         print('Thank you we hoped you enjoyed your party.')
 
+
+def total_revenue():
+    depo = disk.sum_revenue()
+    print("The total revenue for today is $", depo)
+
+
+def manager():
+    passw = "Everest1953"
+    password = input(
+        "Hello manager, can you please type in your password to get the total revenue. \n"
+    )
+    if password == passw:
+        total_revenue()
+    else:
+        print("Sorry wrong password please try again.")
+
+
 def main():
-    print('\n1.\tCheck out store\n2.\tReturn items rented\n')
-    answer = input('What would you like to do today (Please enter number) ?  ')
+    print('\n1.\tCheck out store\n2.\tReturn items rented\n3.\tManager\n')
+    answer = input(
+        'What would you like to do today (Please enter number) ? \n ')
 
     if answer == '1':
         store()
     elif answer == '2':
         returning()
+    elif answer == '3':
+        manager()
     else:
         print("Sorry invalid answer.")
+
+
 if __name__ == '__main__':
     main()
