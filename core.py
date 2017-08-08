445390daeda586324c3cf5a420e3187e947972a4
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-08-08 09:45:08 -0500

    Erased some unnecicarily functions

--- core.py
+++ core.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+
+
 def rent_time(item_name):
     '''str _> float
     Function will recive the item name and it will return
@@ -6,6 +8,8 @@ def rent_time(item_name):
     '''
     time = datetime.now().strftime('%y/%m/%d %H:%M')
     return time
+
+
     # return time.strptime("%H:%M:%S, %B %d, 20%y", ???)
 def get_item(inventory, item):
     ''' list[list], str_> str, float, float
@@ -19,6 +23,7 @@ def get_item(inventory, item):
             return elements[0:3]
     return msg
 
+
 def return_deposit(inventory, number):
     ''' list[str], str _> str 
     Function will get a number from the user and it
@@ -30,6 +35,8 @@ def return_deposit(inventory, number):
         if number in elements:
             return elements[6]
     return msg
+
+
 def item_messages(inventory, item):
     ''' list[list], str_> str
     Function returns a different message if
@@ -43,6 +50,7 @@ def item_messages(inventory, item):
             return good_msg
     return bad_msg
 
+
 def tax_of(money):
     '''float _> float
     Return money multiplied by .07 and 
@@ -53,44 +61,6 @@ def tax_of(money):
     tax = (money) * .07
     total = money + tax
     return '{:0.2f}'.format(total)
-
-# def check_time(inventory,timing_now, number):
-#     ''' int _> float
-#     Returns the difference in time to see 
-#     if the customer owes more or not
-#     '''
-#     msg = 'Sorry there was an error'
-#     for items in inventory:
-#         if number in items:
-#             time = items[5]
-#             diff = timing_now - time
-#             # Divided by 60 twice to get the mintues and hours
-#             difference = diff.total_seconds()/60/60
-#             hours = items[4]
-#             if float(difference) > float(hours):
-#                 x = float(difference) - float(hours)
-#                 return 'You owe', x, 'hours'
-#             return difference
-#     return msg
-
-# def is_on_time(inventory, timing_now, number):
-#     ''' list[str]], date.time, int _> float
-#     Returns the number of hours different
-#     '''
-#     bad_msg = 'Sorry, there was an error during the transaction please try again.'
-#     msg = "You did not exceed the hours, unfortunatly we do not give refunds.  "
-#     for items in inventory:
-#         if number in items:
-#             x = datetime.strptime(items[5], "%d/%m/%y %H:%M")
-#             diff = timing_now - items[5]
-#             hours_diff = diff.total_seconds()/60/60
-#             if hours_diff < timing_now:
-#                 total_hrs = hours_diff - timing_now
-#                 return msg
-#             else:
-#                 return float(total_hrs)
-#     return bad_msg
-
 
 
 # >>> from datetime import datetime
