b1e36ef4a808438561859cbd1ef2013518112bc7
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-08-11 10:41:54 -0500

    Made some successfull pytest for functions in the core

--- test_core.py
+++ test_core.py
@@ -8,9 +8,21 @@ def test_get_item_names():
     assert core.get_item_names(l) == ['string_a', 'string_x']
 
 
+def test_cost_of():
+    actual = core.cost_of(4, 'Chairs', 3.5, [['Chairs', 450, .25, 7.00]])
+    expect = 3.5
+    assert actual == expect
+
+
 def test_get_item():
     actual = core.get_item([['Toys', 20, 2.00]], 'Toys')
     expect = ['Toys', 20, 2.00]
+    assert expect == actual
+
+
+def test_max_amount():
+    actual = core.get_max_amount('Chairs', [['Chairs', '200', '300']])
+    expect = 200
     assert expect == actual
 
 
